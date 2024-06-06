ba40de9f8c4d9036a4491133549bc579145e7bb2
Damdy <damdyjuns@gmail.com>
2024-06-06 13:44:07 +0700

    feat: create google signin button

---
+++ src/components/ui/signInWithGoogle.tsx
@@ -0,0 +1,46 @@
+import { signIn } from "@/auth";
+import prisma from "@/lib/prisma";
+import { AuthError } from "next-auth";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+export function SignInWithGoogle() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+
+        try {
+          const cookie = cookies();
+          const google = await signIn("google");
+          // const user = await prisma.user.findFirst({
+          //   where: { email: google.email },
+          // });
+          // const session = await prisma.session.create({
+          //   data: {
+          //     sessionToken: cookie.get("authjs.session-token"),
+          //     userId: user?.id,
+          //   },
+          // });
+        } catch (error) {
+          if (error instanceof AuthError) {
+            return redirect(`/api/auth?error=${error.type}`);
+          }
+          throw error;
+        }
+      }}
+    >
+      <button
+        type="submit"
+        className="btn bg-white text-black hover:bg-white/80 w-full"
+      >
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          loading="lazy"
+          height={24}
+          width={24}
+        />
+        <span>Sign in with Google</span>
+      </button>
+    </form>
+  );
+}
