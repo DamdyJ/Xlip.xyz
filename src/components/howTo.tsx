75d36e15d5f9457dd89afdab390b302a0cd5db79
Damdy <damdyjuns@gmail.com>
2024-06-06 22:54:49 +0700

    fix: fix mobile responsive issue with grid

--- src/components/howTo.tsx
+++ src/components/howTo.tsx
@@ -4,20 +4,21 @@ import React from "react";
 export default function HowTo() {
   return (
     <>
-      <h2 className="mb-8 text-center text-3xl font-semibold text-white underline underline-offset-8 md:text-5xl">
+      <h2 className="my-8 text-center text-3xl font-semibold text-white underline underline-offset-8 md:text-5xl">
         How to Utilize a Link Shortener
       </h2>
-      <div className="grid gap-4 md:grid-cols-2">
+      <div className="flex w-full flex-col gap-4 md:grid md:grid-cols-2">
         {/* left */}
-        <div className="my-8 md:my-20 place-self-center">
-          <h3 className="text-2xl font-medium">
+        <div className="my-8 place-self-center md:my-20">
+          <h3 className="w-full text-2xl font-medium">
             <span className="font-bold text-white">Step 1: </span>Find Your Long
             Link & Copy It
           </h3>
         </div>
         {/* right */}
-        <div className="my-8 md:mt-20 place-self-center">
+        <div className="my-8 place-self-center md:mt-20">
           <Image
+            className="h-fit w-full"
             src="/link.png"
             alt="Your long link image"
             width={500}
@@ -25,9 +26,10 @@ export default function HowTo() {
           />
         </div>
         {/* left */}
-        <div className="my-8 md:my-20 place-self-center">
+        <div className="my-8 place-self-center md:my-20">
           <div className="hidden md:block">
             <Image
+              className="h-fit w-full"
               src="/generate.png"
               alt="Generate link by our ai"
               width={500}
@@ -42,9 +44,10 @@ export default function HowTo() {
           </div>
         </div>
         {/* right */}
-        <div className="my-8 md:my-20 place-self-center p-4">
+        <div className="my-8 place-self-center md:my-20">
           <div className="block md:hidden">
             <Image
+              className="h-fit w-full"
               src="/generate.png"
               alt="Generate link by our ai"
               width={500}
@@ -59,20 +62,60 @@ export default function HowTo() {
           </div>
         </div>
         {/* left */}
-        <div className="my-8 md:my-20 place-self-center p-4">
+        <div className="my-8 place-self-center md:my-20">
           <h3 className="text-2xl font-medium">
             <span className="font-bold text-white">Step 3: </span>Choose Your
             Favorite Shortened Link & Click to Copy to Your Clipboard
           </h3>
         </div>
         {/* right */}
-        <div className="my-8 md:my-20 place-self-center">
+        <div className="my-8 place-self-center md:my-20">
           <Image
+            className="h-fit w-full"
             src="/output.png"
             alt="Your long link image"
             width={500}
             height={52}
           />
+        </div>
+        {/* left */}
+        <div className="my-8 place-self-center md:my-20">
+          <div className="hidden md:block">
+            <Image
+              className="h-fit w-full"
+              src="/go.png"
+              alt="Generate link by our ai"
+              width={500}
+              height={52}
+            />
+          </div>
+          <div className="block md:hidden">
+            <h3 className="text-2xl font-medium">
+              <span className="font-bold text-white">Step 4: </span>
+              Go to your browser and enter:
+            </h3>
+            <span className="sm:text-xl">https://www.xlip.xyz/go/</span>
+            <span className="text-primary sm:text-xl">your-url</span>
+          </div>
+        </div>
+        {/* right */}
+        <div className="my-8 place-self-center md:my-20">
+          <div className="block md:hidden">
+            <Image
+              className="h-fit w-full"
+              src="/go.png"
+              alt="Generate link by our ai"
+              width={500}
+              height={52}
+            />
+          </div>
+          <div className="hidden md:block">
+            <h3 className="text-2xl font-medium">
+              <span className="font-bold text-white">Step 4: </span>Go to your
+              browser and enter: https://www.xlip.xyz/go/
+              <span className="text-primary">your-choosen-link</span>
+            </h3>
+          </div>
         </div>
       </div>
     </>
