72bed1542e36c1cdad12edcf82dabaa48ab2d1fe
Damdy <damdyjuns@gmail.com>
2024-06-06 19:14:58 +0700

    feat: revamp not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,20 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center justify-center">
+        <div className="flex flex-col gap-4">
+          <h2 className="mb-4 text-3xl font-semibold text-error md:text-5xl text-center">
+            Not Found
+          </h2>
+          <p className="text-white md:text-lg text-center">
+            Could not find requested resource
+          </p>
+          <Link className="btn btn-primary" href="/">
+            Go to homepage
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
