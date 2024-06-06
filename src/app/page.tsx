86ada09a7d2e6a9210548f97be298d1edcabdb61
Damdy <damdyjuns@gmail.com>
2024-06-07 01:18:53 +0700

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import HowTo from "@/components/howTo";
 import Hero from "@/components/hero";
+import Footer from "@/components/footer";
 
 export default async function Home() {
-
   return (
-    <div className="mx-auto w-full max-w-5xl px-4 py-6">
-      <Hero />
-      <HowTo/>
-    </div>
+    <>
+      <div className="mx-auto w-full max-w-5xl px-4 pt-6">
+        <Hero />
+        <HowTo />
+      </div>
+      <Footer />
+    </>
   );
 }
