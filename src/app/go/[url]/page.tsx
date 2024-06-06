0c06198c99c128e9667941bf07d893e64018c0e0
Damdy <damdyjuns@gmail.com>
2024-06-06 19:21:12 +0700

    feat: create redirect page

---
+++ src/app/go/[url]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function RedirectURLPage() {
+  const router = useRouter();
+  const { url } = useParams();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/links/${url}`);
+        const data = await response.json();
+        if (data.link && data.link.redirect) {
+          const redirectTimeout = setTimeout(() => {
+            console.error(
+              "Redirect took too long. Redirecting to not found page...",
+            );
+            router.push("/notfound");
+          }, 5000);
+
+          window.location.href = data.link.redirect;
+
+          window.onload = function () {
+            clearTimeout(redirectTimeout);
+          };
+        } else {
+          console.error("Redirect link not found");
+          router.push("/notfound");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        router.push("/");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (url) {
+      fetchData();
+    }
+  }, [url]);
+
+  if (loading) {
+    return (
+      <div className="flex min-h-screen w-full items-center justify-center">
+        <h1 className="text-3xl font-semibold md:text-5xl">Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <h1 className="text-3xl font-semibold md:text-5xl">Redirecting...</h1>
+    </div>
+  );
+}
