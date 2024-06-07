0c8b55ede18cbe6b34bfaef871a978c68a6cfa6c
Damdy <damdyjuns@gmail.com>
2024-06-07 22:04:52 +0700

    feat: add button loading state and disable it when it happend

--- src/components/ui/magicButton.tsx
+++ src/components/ui/magicButton.tsx
@@ -1,15 +1,24 @@
-export default function MagicButton({ text }: { text: string }) {
+export default function MagicButton({
+  text,
+  isLoading,
+}: {
+  text: string;
+  isLoading: boolean;
+}) {
   return (
     <>
-      <div className="group relative inline-flex z-10">
+      <div className="group relative z-10 inline-flex">
         <div className="transitiona-all animate-tilt absolute -inset-px rounded-xl bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] opacity-70 blur-lg duration-1000 group-hover:-inset-1 group-hover:opacity-100 group-hover:duration-200"></div>
         <button
+          disabled={isLoading}
           type="submit"
-          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] w-full md:w-fit"
+          className="relative inline-flex h-12 w-full overflow-hidden rounded-full p-[1px] disabled:cursor-progress md:w-fit"
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#44BCFF_0%,#FF44EC_50%,#FF675E_75%,#44BCFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            {text}
+          <span
+            className={`inline-flex h-full w-full ${isLoading ? "cursor-loading bg-slate-600" : "cursor-pointer bg-slate-950"} items-center justify-center rounded-full px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl`}
+          >
+            {isLoading ? "Loading..." : text}
           </span>
         </button>
       </div>
