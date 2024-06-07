b3f92e503b90dddc964f2629596f0dbeeca21ead
Damdy <damdyjuns@gmail.com>
2024-06-07 11:37:54 +0700

    reformat: code cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,24 +7,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Xlip.xyz - Effortless Link Shortening",
+  title: "Xlip | Effortless Link Shortener",
   description:
-    "Welcome to Xlip.xyz,your go-to solution for transforming long, cumbersome URLs into concise, shareable links. Powered by cutting-edge technology, our URL shortener not only streamlines your links effortlessly but also provides insightful analytics. Say goodbye to unwieldy URLs and hello to sleek and professional links with advanced analytics at your fingertips.",
-  openGraph: {
-    type: "website",
-    url: "https://www.xlip.xyz/",
-    title: "Xlip.xyz - Effortless Link Shortening",
-    description:
-      "Welcome to Xlip.xyz, your go-to solution for transforming long, cumbersome URLs into concise, shareable links. Powered by cutting-edge technology, our URL shortener not only streamlines your links effortlessly but also provides insightful analytics. Say goodbye to unwieldy URLs and hello to sleek and professional links with advanced analytics at your fingertips.",
-    images: [
-      {
-        url: "https://www.xlip.xyz/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Xlip.xyz - Effortless Link Shortening",
-      },
-    ],
-  },
+    "Xlip | Your go-to solution for transforming long, cumbersome URLs into sleek, shareable links. Powered by cutting-edge technology, our URL shortener offers advanced link management and insightful analytics. Say goodbye to unwieldy URLs and hello to sleek, professional links.",
 };
 
 export default function RootLayout({
@@ -35,19 +20,29 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="night">
       <head>
-        <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.xlip.xyz/" />
-        <meta
-          property="og:title"
-          content="Xlip.xyz - Effortless Link Shortening"
-        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Xlip | Effortless Link Shortener" />
         <meta
           property="og:description"
-          content="Welcome to Xlip.xyz, your go-to solution for transforming long, cumbersome URLs into concise, shareable links. Powered by cutting-edge technology, our URL shortener not only streamlines your links effortlessly but also provides insightful analytics. Say goodbye to unwieldy URLs and hello to sleek and professional links with advanced analytics at your fingertips."
+          content="Xlip | Your go-to solution for transforming long, cumbersome URLs into sleek, shareable links. Powered by cutting-edge technology, our URL shortener offers advanced link management and insightful analytics. Say goodbye to unwieldy URLs and hello to sleek, professional links."
         />
         <meta property="og:image" content="https://www.xlip.xyz/og-image.png" />
         <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:image:height" content="631" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="xlip.xyz" />
+        <meta property="twitter:url" content="https://www.xlip.xyz/" />
+        <meta name="twitter:title" content="Xlip | Effortless Link Shortener" />
+        <meta
+          name="twitter:description"
+          content="Xlip | Your go-to solution for transforming long, cumbersome URLs into sleek, shareable links. Powered by cutting-edge technology, our URL shortener offers advanced link management and insightful analytics. Say goodbye to unwieldy URLs and hello to sleek, professional links."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.xlip.xyz/og-image.png"
+        />
       </head>
       <body className={inter.className}>
         {children}
