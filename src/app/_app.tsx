c3885599673a49861e48f04799d18f1857c55ae8
Damdy <damdyjuns@gmail.com>
2024-06-07 14:42:30 +0700

    fix: fix script id problem

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,5 +1,6 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
@@ -9,7 +10,19 @@ export default function App({
       <SessionProvider session={session} basePath="/api/auth">
         <Component {...pageProps} />
       </SessionProvider>
+      <Script
+        id="google-analytics"
+        src="https://www.googletagmanager.com/gtag/js?id=G-DQ3HZK6V4G"
+        strategy="afterInteractive"
+      >
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
+            gtag('config', 'G-DQ3HZK6V4G');
+          `}
+      </Script>
     </>
   );
 }
