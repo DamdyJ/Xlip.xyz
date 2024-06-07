3207e293027c966369ac629b53508f4825a50233
Damdy <damdyjuns@gmail.com>
2024-06-07 14:48:53 +0700

    feat: add nextjs google analytic package

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,28 +1,17 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
     <>
-      <SessionProvider session={session} basePath="/api/auth">
+      <GoogleAnalytics trackPageViews />
+      {/* <SessionProvider session={session} basePath="/api/auth"> */}
         <Component {...pageProps} />
-      </SessionProvider>
-      <Script
-        id="google-analytics"
-        src="https://www.googletagmanager.com/gtag/js?id=G-DQ3HZK6V4G"
-        strategy="afterInteractive"
-      >
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-DQ3HZK6V4G');
-          `}
-      </Script>
+      {/* </SessionProvider> */}
     </>
   );
 }
