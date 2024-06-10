747ecae030db2c2530b0235cfb0696ad51a5ff13
Damdy <damdyjuns@gmail.com>
2024-06-10 12:34:23 +0700

    feat: add link counter to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HowTo from "@/components/howTo";
 import Hero from "@/components/hero";
 import Footer from "@/components/footer";
+import LinkCounter from "@/components/link-counter";
 
 export default async function Home() {
   return (
     <>
       <div className="mx-auto w-full max-w-5xl px-4 pt-6">
         <Hero />
+        <LinkCounter/>  
         <HowTo />
       </div>
       <Footer />
