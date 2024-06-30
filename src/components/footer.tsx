eb327bae5a7a1149dab91c789c18fcd46f7533f6
Damdy <damdyjuns@gmail.com>
2024-06-30 07:13:03 +0700

    style: v2 footer redesign

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,60 +1,53 @@
-import React from "react";
-import Avatar from "./ui/avatar";
 import Link from "next/link";
-import Image from "next/image";
+import XlipIcon from "./icons/logo";
 
 export default function Footer() {
   return (
-    <>
-      <footer className="min-h-20 bg-slate-950">
-        <div className="mx-auto w-full max-w-5xl px-4 py-8">
-          <div className="mb-4 flex flex-col items-center gap-3 text-center">
-            <div className="flex gap-2 items-center">
-              <Image
-                src="/xlip-logo-white.svg"
-                alt="xlip logo"
-                width={16}
-                height={16}
-                className="h-4 w-4"
-              />
-              <h4 className="text-lg font-bold text-white">Xlip</h4>
-            </div>
-            <Avatar />
-            <h5 className="md:w-1/2">
-              Hey there! 👋 I&apos;m Damdy, the developer behind Xlip. You can
-              follow me 👇
-            </h5>
-          </div>
-          <div className="mb-3 flex justify-center gap-4">
-            <div className="tooltip z-10" data-tip="github/DamdyJ">
-              <Link href={"https://github.com/DamdyJ"}>
-                <Image
-                  src="/github.svg"
-                  alt="icon"
-                  className="h-10 w-10"
-                  width={24}
-                  height={24}
-                />
-              </Link>
-            </div>
-            <div className="tooltip z-10" data-tip="linkedIn/damdy-junaedi">
-              <Link href={"https://www.linkedin.com/in/damdy-junaedi"}>
-                <Image
-                  src="/linkedIn.svg"
-                  alt="icon"
-                  className="h-10 w-10"
-                  width={24}
-                  height={24}
-                />
-              </Link>
-            </div>
-          </div>
-
-          <div className="text-center text-xs">
-            <p>Copyright © 2024 - All rights reserved.</p>
-          </div>
-        </div>
-      </footer>
-    </>
+    <footer className="min-h-[17.88rem] bg-[#120E14] font-bold leading-[120%] text-white">
+      <div className="container mx-auto max-w-5xl space-y-8 px-4 pb-11 pt-8">
+        <nav className="flex gap-[103px]">
+          <section className="flex max-w-[401px] flex-col justify-between rounded-lg bg-[#1E0E2B] p-4">
+            <XlipIcon />
+            <p className="text-white/50">
+              XLip simplifies your digital interactions by offering smart URL
+              shortening, dynamic QR code generation, and insightful link
+              analytics—all in one platform.
+            </p>
+          </section>
+          <section className="w-full max-w-[157px] space-y-4">
+            <h5 className="text-white/50">NAVIGATION</h5>
+            <ul className="space-y-4">
+              <li>
+                <Link href="#features">Features</Link>
+              </li>
+              <li>
+                <Link href="#how-does-it-work">How does it work</Link>
+              </li>
+              <li>
+                <Link href="#try-it-out">Try it out</Link>
+              </li>
+              <li>
+                <Link href="#faq">FAQ</Link>
+              </li>
+            </ul>
+          </section>
+          <section className="w-full max-w-[157px] space-y-4">
+            <h5 className="text-white/50">LET&apos;S CONNECT</h5>
+            <ul className="space-y-4">
+              <li>
+                <Link href="#features">Contact me</Link>
+              </li>
+              <li>
+                <Link href="#how-does-it-work">LinkedIn</Link>
+              </li>
+              <li>
+                <Link href="#try-it-out">Github</Link>
+              </li>
+            </ul>
+          </section>
+        </nav>
+        <p className="">Copyright © 2024 All Rights Reserved.</p>
+      </div>
+    </footer>
   );
 }
