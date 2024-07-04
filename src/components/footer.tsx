facf8c45b99386fd25701591ddc5988750825e57
Damdy <damdyjuns@gmail.com>
2024-07-04 21:42:57 +0700

    style: restyling the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,10 @@ import XlipIcon from "./icons/logo";
 
 export default function Footer() {
   return (
-    <footer className="min-h-[17.88rem] bg-[#120E14] font-bold leading-[120%] text-white">
+    <footer className="bg-[#120E14] font-bold leading-[120%] text-white relative z-20">
       <div className="container mx-auto max-w-5xl space-y-8 px-4 pb-11 pt-8">
-        <nav className="flex gap-[103px]">
-          <section className="flex max-w-[401px] flex-col justify-between rounded-lg bg-[#1E0E2B] p-4">
+        <nav className="md:flex-row md:justify-between lg:gap-[103px] flex flex-col gap-6">
+          <section className="lg:justify-between md:mx-0 mx-auto flex max-w-[401px] flex-col gap-4 rounded-lg bg-[#1E0E2B] p-4">
             <XlipIcon />
             <p className="text-white/50">
               XLip simplifies your digital interactions by offering smart URL
@@ -14,7 +14,7 @@ export default function Footer() {
               analytics—all in one platform.
             </p>
           </section>
-          <section className="w-full max-w-[157px] space-y-4">
+          <section className="md:text-left md:self-start w-full max-w-[157px] space-y-4 self-center text-center">
             <h5 className="text-white/50">NAVIGATION</h5>
             <ul className="space-y-4">
               <li>
@@ -31,22 +31,31 @@ export default function Footer() {
               </li>
             </ul>
           </section>
-          <section className="w-full max-w-[157px] space-y-4">
+          <section className="md:text-left md:self-start w-full max-w-[157px] space-y-4 self-center text-center">
             <h5 className="text-white/50">LET&apos;S CONNECT</h5>
             <ul className="space-y-4">
               <li>
-                <Link href="#features">Contact me</Link>
+                <Link href="/contact-me">Contact me</Link>
               </li>
               <li>
-                <Link href="#how-does-it-work">LinkedIn</Link>
+                <Link
+                  href="https://www.linkedin.com/in/damdy-junaedi/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </Link>
               </li>
               <li>
-                <Link href="#try-it-out">Github</Link>
+                <Link href="https://github.com/DamdyJ" target="_blank">
+                  Github
+                </Link>
               </li>
             </ul>
           </section>
         </nav>
-        <p className="">Copyright © 2024 All Rights Reserved.</p>
+        <p className="md:text-left text-center text-white/50">
+          Copyright © <time dateTime="2024">2024</time> All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
