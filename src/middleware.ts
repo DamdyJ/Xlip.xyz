a98064d44b8da66f9be6c900163b8977f864528d
Damdy <damdyjuns@gmail.com>
2024-07-04 21:52:34 +0700

    feat: add whitelist on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,27 @@
-import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
 
-export default auth((req) => {
-  if (!req.auth) {
-    const newUrl = new URL("/signin", req.nextUrl.origin);
-    return Response.redirect(newUrl);
+const { auth } = NextAuth(authConfig);
+
+// TODO: add protected route
+export default auth(async function middleware(req: NextRequest) {
+  const isWhitelisted = whitelist.some((path) =>
+    req.nextUrl.pathname.startsWith(path),
+  );
+
+  if (isWhitelisted) {
+    return NextResponse.next();
+  }
+
+  const session = await auth();
+  if (!session) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
+const whitelist = ["/"];
