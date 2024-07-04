f0b005692f1929badb1553ccbcb3bab866bcbdb0
Damdy <damdyjuns@gmail.com>
2024-07-04 21:40:44 +0700

    feat: auth logic with prisma adapter and jwt session token

--- src/auth.ts
+++ src/auth.ts
@@ -1,35 +1,12 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import GitHub from "next-auth/providers/github";
+import authConfig from "./auth.config";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
-import type { Provider } from "next-auth/providers";
-
-const providers: Provider[] = [Google, GitHub];
-
-export const providerMap = providers.map((provider) => {
-  if (typeof provider === "function") {
-    const providerData = provider();
-    return { id: providerData.id, name: providerData.name };
-  } else {
-    return { id: provider.id, name: provider.name };
-  }
-});
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers,
-  pages: {
-    signIn: "/signin",
-    signOut: "/signout",
-  },
-  secret: process.env.AUTH_SECRET,
-  session: {
-    strategy: "database",
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return baseUrl;
-    },
-  },
+  session: { strategy: "jwt" },
+  ...authConfig,
+  secret: process.env.AUTH_SECRET!,
 });
